89ef4b6826d31099ba7fde174eca15889f2b9fb5
Greg Butt <xavierbutt@gmail.com>
2022-12-09 09:00:08 -0700

    add pre-commit hooks and editorconfig

---
+++ 2022/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "jest"],
+  rules: {
+    "no-var": "error",
+    "brace-style": "error",
+    "prefer-template": "error",
+    radix: "error",
+    "space-before-blocks": "error",
+    "import/prefer-default-export": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+  overrides: [
+    {
+      files: ["**/*.test.{js,ts}", "**/*.spec.{js,ts}"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
--- 2022/tests/day1.test.ts
+++ 2022/tests/day1.test.ts
@@ -2,20 +2,22 @@ import fs from "fs/promises";
 
 // https://adventofcode.com/2022/day/1
 describe("Day 1 - Calorie Counting", () => {
+  let input: string;
+  beforeAll(async () => {
+    input = (await fs.readFile("tests/inputs/day1.input")).toString();
+  });
   it("Example", () => {
     const { maxCalories, topThreeCalories } = findMaxCalories(exampleInput);
     expect(maxCalories).toBe(24000);
     expect(topThreeCalories).toBe(45000);
   });
 
-  it("Part 1", async () => {
-    const input = (await fs.readFile("tests/inputs/day1.input")).toString();
+  it("Part 1", () => {
     const { maxCalories } = findMaxCalories(input);
     expect(maxCalories).toBe(68787);
   });
 
-  it("Part 2", async () => {
-    const input = (await fs.readFile("tests/inputs/day1.input")).toString();
+  it("Part 2", () => {
     const { topThreeCalories } = findMaxCalories(input);
     expect(topThreeCalories).toBe(198041);
   });
@@ -25,7 +27,7 @@ function findMaxCalories(input: string) {
   const elfCalorieList = input.split("\n").reduce(
     (acc, calorieStr) => {
       if (calorieStr) {
-        acc[acc.length - 1] += parseInt(calorieStr);
+        acc[acc.length - 1] += parseInt(calorieStr, 10);
       } else {
         acc.push(0);
       }
